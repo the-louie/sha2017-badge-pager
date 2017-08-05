1334d2ac67add59a49f62698db3a2f30c8c255b4
Anders Green <louie@louie.se>
2017-08-05 13:35:48 +0200

    correct dirnames

--- booter/__init__.py
+++ booter/__init__.py
@@ -1,16 +1,19 @@
 import shell
-
-
+import urequests as requests
+import io, os
 import network
-sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
+
+sta_if = network.WLAN(network.STA_IF);
+sta_if.active(True)
 sta_if.scan()
 sta_if.connect("SHA2017-insecure")
 
-import urequests as requests
-r = requests.get('https://louie.se/dumpster/code/sha2017/__init__.py')
+r = requests.get('https://github.com/the-louie/sha2017-badge-pager/blob/master/__init__.py')
 
-import io
-f = io.open('/lib/mash-test/__init__.py', 'w')
+if not os.path.isdir('lib/louie-test'):
+    os.mkdir('/lib/louie-test')
+
+f = io.open('/lib/louie-test/__init__.py', 'w')
 f.write(r.text)
 f.close()
 
