8146b7e40f4476fb4602e1e88c94610f0506de2a
Anders Green <louie@louie.se>
2017-08-06 16:49:40 +0200

    betterbranch

--- booter/__init__.py
+++ booter/__init__.py
@@ -17,7 +17,8 @@ sta_if.connect("SHA2017-insecure")
 
 import io
 import urequests as requests
-r = requests.get('https://raw.githubusercontent.com/the-louie/sha2017-badge-pager/master/__init__.py')
+r = requests.get('https://raw.githubusercontent.com/the-louie/sha2017-badge-pager/betterselector/__init__.py')
+# r = requests.get('https://raw.githubusercontent.com/the-louie/sha2017-badge-pager/master/__init__.py')
 
 f = io.open('/lib/louie-test/__init__.py', 'w')
 f.write(r.text)
